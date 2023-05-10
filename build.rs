13b197a743274a3e1645a4faf681e99103cf0d01
Gyusun Yeom <omniavinco@gmail.com>
2023-05-10 13:29:58 +0900

    Remove build.rs

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    if cfg!(feature = "embed_web") {
-        std::process::Command::new("sh")
-            .args(["-C", "npx webpack"])
-            .env("PRODUCTION", "1")
-            .output()
-            .unwrap();
-    }
-}
